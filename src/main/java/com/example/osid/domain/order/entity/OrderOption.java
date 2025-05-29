298af99bb9ceaf5ad80261439fccdbad395cea5a
songgaeun <rkdmsdmqek1128@gmail.com>
2025-05-29 16:53:15 +0900

    feat: 주문 옵션 중간 테이블 생성

---
+++ src/main/java/com/example/osid/domain/order/entity/OrderOption.java
@@ -0,0 +1,36 @@
+package com.example.osid.domain.order.entity;
+
+import com.example.osid.domain.option.entity.Option;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "order_option")
+public class OrderOption {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "order_id")
+	private Order order;
+
+	@ManyToOne
+	@JoinColumn(name = "option_id")
+	private Option option;
+}
