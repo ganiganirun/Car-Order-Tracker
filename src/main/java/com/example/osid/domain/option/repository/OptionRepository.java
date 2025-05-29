c61ab16f8406027d906fc88f0f2f87d19a9dbc90
songgaeun <rkdmsdmqek1128@gmail.com>
2025-05-29 16:52:19 +0900

    feat: 옵션리스트 반환

--- src/main/java/com/example/osid/domain/option/repository/OptionRepository.java
+++ src/main/java/com/example/osid/domain/option/repository/OptionRepository.java
@@ -1,10 +1,16 @@
 package com.example.osid.domain.option.repository;
 
-import com.example.osid.domain.option.entity.Option;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.osid.domain.option.entity.Option;
+
 @Repository
 public interface OptionRepository extends JpaRepository<Option, Long> {
+
+	// option list 반환
+	List<Option> findByIdIn(List<Long> ids);
 
 }
