543ad8f0c56f2cfeb36b0aa8b713714147c7dbd6
songgaeun <rkdmsdmqek1128@gmail.com>
2025-05-31 23:47:31 +0900

    feat: orderResponse dto

---
+++ src/main/java/com/example/osid/domain/order/dto/response/OrderResponseDto.java
@@ -0,0 +1,155 @@
+package com.example.osid.domain.order.dto.response;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.example.osid.domain.order.enums.OrderStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class OrderResponseDto {
+
+	@Builder
+	@Getter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class Add {
+
+		private Long id;
+
+		private String userName;
+
+		private String dealerName;
+
+		private List<String> orderOptions;
+
+		private String model;
+
+		private String address;
+
+		private Long totalPrice;
+
+		private OrderStatus orderStatus; // 주문 상태
+
+		private LocalDateTime createdAt;
+
+	}
+
+	@Builder
+	@Getter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class Update {
+
+		private Long id;
+
+		private String userName;
+
+		private String dealerName;
+
+		private List<String> orderOptions;
+
+		private String model;
+
+		private String address;
+
+		private Long totalPrice;
+
+		private OrderStatus orderStatus; // 주문 상태
+
+		private LocalDate expectedDeliveryAt; // 예상 출고일
+
+		private LocalDate actualDeliveryAt; // 실제 출고일
+
+		private LocalDateTime createdAt;
+
+	}
+
+	@Builder
+	@Getter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class AdminView {
+
+		private Long id;
+
+		private String userName;
+
+		private String dealerName;
+
+		private List<String> orderOptions;
+
+		private String model;
+
+		private String address;
+
+		private Long totalPrice;
+
+		private OrderStatus orderStatus; // 주문 상태
+
+		private LocalDate expectedDeliveryAt; // 예상 출고일
+
+		private LocalDate actualDeliveryAt; // 실제 출고일
+
+		private LocalDateTime createdAt;
+
+	}
+
+	@Builder
+	@Getter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class UserView {
+
+		private Long id;
+
+		private String userName;
+
+		private String dealerName;
+
+		private List<String> orderOptions;
+
+		private String model;
+
+		private String address;
+
+		private Long totalPrice;
+
+		private OrderStatus orderStatus; // 주문 상태
+
+		private LocalDate expectedDeliveryAt; // 예상 출고일
+
+		private LocalDate actualDeliveryAt; // 실제 출고일
+
+		private LocalDateTime createdAt;
+
+	}
+
+	@Builder
+	@Getter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class FindAll {
+
+		private Long id;
+
+		private String userName;
+
+		private String dealerName;
+
+		private String model;
+
+		// private Long totalPrice;
+		//
+		// private OrderStatus orderStatus; // 주문 상태
+		//
+		// private LocalDate expectedDeliveryAt; // 예상 출고일
+		//
+		// private LocalDate actualDeliveryAt; // 실제 출고일
+
+	}
+}
