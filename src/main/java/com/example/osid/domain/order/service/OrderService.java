77450fafbe766b32ebd6b1c8d8cd4009d9ed1f0a
songgaeun <rkdmsdmqek1128@gmail.com>
2025-05-31 23:46:11 +0900

    feat: order CRUD

--- src/main/java/com/example/osid/domain/order/service/OrderService.java
+++ src/main/java/com/example/osid/domain/order/service/OrderService.java
@@ -1,13 +1,30 @@
 package com.example.osid.domain.order.service;
 
-import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.osid.common.entity.enums.Role;
+import com.example.osid.common.exception.CustomException;
+import com.example.osid.common.exception.ErrorCode;
+import com.example.osid.domain.dealer.entity.Dealer;
 import com.example.osid.domain.dealer.repository.DealerRepository;
+import com.example.osid.domain.model.entity.Model;
 import com.example.osid.domain.model.repository.ModelRepository;
+import com.example.osid.domain.option.entity.Option;
 import com.example.osid.domain.option.repository.OptionRepository;
 import com.example.osid.domain.order.dto.request.OrderRequestDto;
-import com.example.osid.domain.order.repository.OrderOptionRepository;
+import com.example.osid.domain.order.dto.response.OrderResponseDto;
+import com.example.osid.domain.order.entity.OrderOption;
+import com.example.osid.domain.order.entity.Orders;
+import com.example.osid.domain.order.enums.OrderStatus;
 import com.example.osid.domain.order.repository.OrderRepository;
+import com.example.osid.domain.order.repository.OrderSearch;
+import com.example.osid.domain.user.entity.User;
 import com.example.osid.domain.user.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -21,25 +38,213 @@ public class OrderService {
 	private final OrderRepository orderRepository;
 	private final OptionRepository optionRepository;
 	private final ModelRepository modelRepository;
-	private final OrderOptionRepository orderOptionRepository;
 	private final UserRepository userRepository;
 	private final DealerRepository dealerRepository;
+	private final OrderSearch orderSearch;
 
-	public void createOrder(Long dealerId, OrderRequestDto.Add requestDto) {
+	// 주문 생성
+	public OrderResponseDto.Add createOrder(Long dealerId, OrderRequestDto.Add requestDto) {
 		/*
 		 * dealerId로 딜러 가져오기
 		 * 유저 이메일로 유저 가져오기
-		 * 옵션 리스트로 옵션에서 찾아옴
-		 * 모델 id으로 모델 객체 가져와야함
-		 * 옵션리스트로 총가격 계산하고
+		 * 옵션 id 리스트로 옵션에서 가져오기
+		 * 모델 id로 모델 객체 가져오기
+		 * 옵션리스트로 옵션가격 계산
+		 * 차량 고유번호 생성
+		 * 총 가격 계산
+		 * order 객체 생성
+		 * orderoption 생성
 		 * order 객체 저장
-		 * orderoption 생성 및 저장
-		 *
 		 * */
 
-		// Optional<Dealer> byId = dealerRepository.findById(dealerId);
-		// Optional<User> byEmail = userRepository.findByEmail(requestDto.getUserEmail());
-		// Optional<Model> byId = modelRepository.findById(requestDto.getModelId());
-		// optionRepository.findByIdIn(requestDto.getOption());
+		Dealer dealer = dealerRepository.findById(dealerId)
+			.orElseThrow(() -> new CustomException(ErrorCode.INVALID_INPUT_VALUE));
+
+		User user = userRepository.findByEmail(requestDto.getUserEmail())
+			.orElseThrow(() -> new CustomException(ErrorCode.INVALID_INPUT_VALUE));
+
+		Model model = modelRepository.findById(requestDto.getModelId())
+			.orElseThrow(() -> new CustomException(ErrorCode.INVALID_INPUT_VALUE));
+
+		List<Option> options = optionRepository.findByIdIn(requestDto.getOption());
+
+		// 총 금액 계산
+		Long totalPrice = options.stream().mapToLong(Option::getPrice).sum() + model.getPrice();
+
+		// 차량 고유 번호 생성
+		String bodyNumber = UUID.randomUUID().toString();
+
+		Orders orders = Orders.builder()
+			.address(requestDto.getAddress())
+			.bodyNumber(bodyNumber)
+			.totalPrice(totalPrice)
+			.orderStatus(OrderStatus.ORDERED)
+			.user(user)
+			.dealer(dealer)
+			.model(model)
+			.build();
+		
+		List<OrderOption> orderOptions = options
+			.stream()
+			.map(option -> new OrderOption(orders, option))
+			.toList();
+
+		orders.setOrderOptions(orderOptions);
+
+		orderRepository.save(orders);
+
+		// Option 이름만 리스트화
+		List<String> optionNames = options
+			.stream()
+			.map(Option::getName)
+			.toList();
+
+		return OrderResponseDto.Add.builder()
+			.id(orders.getId())
+			.model(orders.getModel().getName())
+			.userName(orders.getUser().getName())
+			.dealerName(orders.getDealer().getName())
+			.orderOptions(optionNames)
+			.address(orders.getAddress())
+			.totalPrice(orders.getTotalPrice())
+			.orderStatus(orders.getOrderStatus())
+			.createdAt(orders.getCreatedAt())
+			.build();
+
+	}
+
+	// 주문 수정
+	@Transactional
+	public OrderResponseDto.Update updateOrder(Long orderId, OrderRequestDto.Update requestDto) {
+
+		Orders orders = orderRepository.findById(orderId)
+			.orElseThrow(() -> new CustomException(ErrorCode.INVALID_INPUT_VALUE));
+
+		// 유동적으로 바꾸기 위해서 if문으로 관리
+		// 추후 다른 방법이 생기면 바뀔 예정
+
+		if (requestDto.getAddress() != null) {
+			orders.setAddress(requestDto.getAddress());
+		}
+
+		if (requestDto.getOrderStatus() != null) {
+			orders.setOrderStatus(requestDto.getOrderStatus());
+		}
+
+		if (requestDto.getExpectedDeliveryAt() != null) {
+			orders.setExpectedDeliveryAt(requestDto.getExpectedDeliveryAt());
+		}
+
+		if (requestDto.getActualDeliveryAt() != null) {
+			orders.setActualDeliveryAt(requestDto.getActualDeliveryAt());
+		}
+
+		List<String> optionNames = orders.getOrderOptions().stream()
+			.map(OrderOption::getOption)
+			.map(Option::getName)
+			.toList();
+
+		return OrderResponseDto.Update.builder()
+			.id(orders.getId())
+			.userName(orders.getUser().getName())
+			.model(orders.getModel().getName())
+			.dealerName(orders.getDealer().getName())
+			.orderOptions(optionNames)
+			.address(orders.getAddress())
+			.totalPrice(orders.getTotalPrice())
+			.orderStatus(orders.getOrderStatus())
+			.expectedDeliveryAt(orders.getExpectedDeliveryAt())
+			.actualDeliveryAt(orders.getActualDeliveryAt())
+			.createdAt(orders.getCreatedAt())
+			.build();
+
+	}
+
+	public Object findOrder(Role role, Long id, Long orderId) {
+
+		Orders orders = orderRepository.findById(orderId)
+			.orElseThrow(() -> new CustomException(ErrorCode.INVALID_INPUT_VALUE));
+
+		List<String> optionNames = orders.getOrderOptions().stream()
+			.map(OrderOption::getOption)
+			.map(Option::getName)
+			.toList();
+
+		switch (role) {
+			case USER -> {
+				if (!orders.getUser().getId().equals(id)) {
+					throw new CustomException(ErrorCode.FORBIDDEN); // 유저는 자신의 주문에 대해서만 조회 가능
+				}
+				return OrderResponseDto.UserView.builder()
+					.id(orders.getId())
+					.userName(orders.getUser().getName())
+					.dealerName(orders.getDealer().getName())
+					.model(orders.getModel().getName())
+					.orderOptions(optionNames)
+					.address(orders.getAddress())
+					.totalPrice(orders.getTotalPrice())
+					.orderStatus(orders.getOrderStatus())
+					.expectedDeliveryAt(orders.getExpectedDeliveryAt())
+					.actualDeliveryAt(orders.getActualDeliveryAt())
+					.createdAt(orders.getCreatedAt())
+					.build();
+			}
+			case DEALER -> {
+				if (!orders.getDealer().getId().equals(id)) {
+					throw new CustomException(ErrorCode.FORBIDDEN); // 딜러는 자신의 주문에 대해서만 조회 가능
+				}
+				return OrderResponseDto.AdminView.builder()
+					.id(orders.getId())
+					.userName(orders.getUser().getName())
+					.dealerName(orders.getDealer().getName())
+					.model(orders.getModel().getName())
+					.orderOptions(optionNames)
+					.address(orders.getAddress())
+					.totalPrice(orders.getTotalPrice())
+					.orderStatus(orders.getOrderStatus())
+					.expectedDeliveryAt(orders.getExpectedDeliveryAt())
+					.actualDeliveryAt(orders.getActualDeliveryAt())
+					.createdAt(orders.getCreatedAt())
+					.build();
+			}
+			case MASTER -> {
+				// 마스터는 모든 주문에 대해서 조회 가능
+				return OrderResponseDto.AdminView.builder()
+					.id(orders.getId())
+					.model(orders.getModel().getName())
+					.userName(orders.getUser().getName())
+					.dealerName(orders.getDealer().getName())
+					.orderOptions(optionNames)
+					.address(orders.getAddress())
+					.totalPrice(orders.getTotalPrice())
+					.orderStatus(orders.getOrderStatus())
+					.expectedDeliveryAt(orders.getExpectedDeliveryAt())
+					.actualDeliveryAt(orders.getActualDeliveryAt())
+					.createdAt(orders.getCreatedAt())
+					.build();
+			}
+			default -> throw new CustomException(ErrorCode.FORBIDDEN); // 정의되지 않은 역할
+		}
+
+	}
+
+	// 주문 삭제
+	public void deleteOrder(Long orderId) {
+
+		Orders orders = orderRepository.findById(orderId)
+			.orElseThrow(() -> new CustomException(ErrorCode.INVALID_INPUT_VALUE));
+
+		orderRepository.delete(orders);
+
+	}
+
+	public Page<OrderResponseDto.FindAll> findAllOrder(Role role, Long id, Pageable pageable) {
+
+		Page<Orders> orders = orderSearch.findOrderAll(role, pageable, id);
+
+		return orders.map(
+			order -> new OrderResponseDto.FindAll(order.getId(), order.getUser().getName(), order.getDealer().getName(),
+				order.getModel().getName()));
+
 	}
 }
