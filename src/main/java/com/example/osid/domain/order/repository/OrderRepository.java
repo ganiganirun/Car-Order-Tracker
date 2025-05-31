9517160451535a06062eb972d0d3bb8d43240c60
songgaeun <rkdmsdmqek1128@gmail.com>
2025-05-31 23:48:44 +0900

    fix: 클래스명 중복 변경

--- src/main/java/com/example/osid/domain/order/repository/OrderRepository.java
+++ src/main/java/com/example/osid/domain/order/repository/OrderRepository.java
@@ -1,8 +1,9 @@
 package com.example.osid.domain.order.repository;
 
-import com.example.osid.domain.order.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {
+import com.example.osid.domain.order.entity.Orders;
+
+public interface OrderRepository extends JpaRepository<Orders, Long> {
 
 }
