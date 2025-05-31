ca9c6676126be600681ab3f7f36e2a7621551c46
songgaeun <rkdmsdmqek1128@gmail.com>
2025-05-31 23:44:08 +0900

    feat: id 자동 생성 수정

--- src/main/java/com/example/osid/domain/user/entity/User.java
+++ src/main/java/com/example/osid/domain/user/entity/User.java
@@ -1,12 +1,18 @@
 package com.example.osid.domain.user.entity;
 
+import java.time.LocalDate;
+
 import com.example.osid.common.entity.BaseEntity;
 import com.example.osid.common.entity.enums.Role;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,28 +26,30 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class User extends BaseEntity {
 
-    @Id
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Column(nullable = false)
-    private String email; //이메일
+	@Column(nullable = false)
+	private String email; //이메일
 
-    @Column(nullable = false)
-    private String password; //비밀번호
+	@Column(nullable = false)
+	private String password; //비밀번호
 
-    @Column(nullable = false)
-    private String name; //이름
+	@Column(nullable = false)
+	private String name; //이름
 
-    @Column(nullable = false)
-    private LocalDate dateOfBirth; //생년월일
+	@Column(nullable = false)
+	private LocalDate dateOfBirth; //생년월일
 
-    @Column(nullable = false)
-    private String phoneNumber; //전화번호
+	@Column(nullable = false)
+	private String phoneNumber; //전화번호
 
-    @Column(nullable = false)
-    private String address; //주소
+	@Column(nullable = false)
+	private String address; //주소
 
-    @Column(nullable = false)
-    private Role role;
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private Role role;
 
 }
