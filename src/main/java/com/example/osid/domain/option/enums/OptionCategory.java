deb574f492b2abe9a75b7c76c4efa33b69571c29
songgaeun <rkdmsdmqek1128@gmail.com>
2025-05-31 23:49:21 +0900

    test: test위해서 임의로 수정

--- src/main/java/com/example/osid/domain/option/enums/OptionCategory.java
+++ src/main/java/com/example/osid/domain/option/enums/OptionCategory.java
@@ -1,4 +1,9 @@
 package com.example.osid.domain.option.enums;
 
 public enum OptionCategory {
+	CONVENIENCE,   // 편의
+	SAFETY,        // 안전
+	EXTERIOR,      // 외관
+	INFOTAINMENT,  // 인포테인먼트
+	PERFORMANCE    // 성능
 }
