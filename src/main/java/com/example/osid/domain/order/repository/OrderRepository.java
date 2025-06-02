b0e7bebc3a92d49d606072fa9b8fb68583077d50
songgaeun <rkdmsdmqek1128@gmail.com>
2025-06-02 18:37:33 +0900

    feat: 주문번호로 order 객체 찾기

--- src/main/java/com/example/osid/domain/order/repository/OrderRepository.java
+++ src/main/java/com/example/osid/domain/order/repository/OrderRepository.java
@@ -1,9 +1,12 @@
 package com.example.osid.domain.order.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.osid.domain.order.entity.Orders;
 
 public interface OrderRepository extends JpaRepository<Orders, Long> {
 
+	Optional<Orders> findByMerchantUid(String merchantUid);
 }
