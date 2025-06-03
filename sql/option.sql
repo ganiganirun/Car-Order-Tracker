422595aef11014b0e078776a8fc8944a3f2ca7a1
songgaeun <rkdmsdmqek1128@gmail.com>
2025-06-03 14:08:22 +0900

    docs: 결제 실험 위해서 가격 변동

--- sql/option.sql
+++ sql/option.sql
@@ -7,9 +7,9 @@ INSERT INTO options (id,
                      created_at,
                      updated_at,
                      deleted_at)
-VALUES (1, '네비1', '옵션 설명1', '/imageURL.png', 'NAVIGATION', 100000, NOW(), NOW(), NULL),
+VALUES (1, '네비1', '옵션 설명1', '/imageURL.png', 'NAVIGATION', 1000, NOW(), NOW(), NULL),
 
-       (2, '선루프', '옵션 설명2', '/imageURL.png', 'SUNROOF', 400000, NOW(), NOW(), NULL),
+       (2, '선루프', '옵션 설명2', '/imageURL.png', 'SUNROOF', 4000, NOW(), NOW(), NULL),
 
-       (3, '주차 센서', '옵션 설명3', '/imageURL.png', 'PARKING_SENSOR', 500000, NOW(), NOW(),
+       (3, '주차 센서', '옵션 설명3', '/imageURL.png', 'PARKING_SENSOR', 5000, NOW(), NOW(),
         NULL);
