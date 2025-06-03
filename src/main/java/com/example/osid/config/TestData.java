797d54373cc3f2040e4c160439c10fcf4f7515b9
songgaeun <rkdmsdmqek1128@gmail.com>
2025-06-03 14:07:16 +0900

    remove: 삭제 예정

--- src/main/java/com/example/osid/config/TestData.java
+++ src/main/java/com/example/osid/config/TestData.java
@@ -7,13 +7,8 @@
 // import com.example.osid.common.entity.enums.Role;
 // import com.example.osid.domain.dealer.entity.Dealer;
 // import com.example.osid.domain.dealer.repository.DealerRepository;
-// import com.example.osid.domain.model.entity.Model;
-// import com.example.osid.domain.model.enums.ModelCategory;
-// import com.example.osid.domain.model.enums.ModelColor;
-// import com.example.osid.domain.model.repository.ModelRepository;
-// import com.example.osid.domain.option.entity.Option;
-// import com.example.osid.domain.option.enums.OptionCategory;
-// import com.example.osid.domain.option.repository.OptionRepository;
+// import com.example.osid.domain.master.entity.Master;
+// import com.example.osid.domain.master.repository.MasterRepository;
 // import com.example.osid.domain.user.entity.User;
 // import com.example.osid.domain.user.repository.UserRepository;
 //
@@ -26,11 +21,23 @@
 //
 // 	private final DealerRepository dealerRepository;
 // 	private final UserRepository userRepository;
-// 	private final ModelRepository modelRepository;
-// 	private final OptionRepository optionRepository;
+// 	private final MasterRepository masterRepository;
 //
 // 	@PostConstruct
 // 	public void initTestData() {
+//
+// 		masterRepository.save(
+// 			Master.builder()
+// 				.businessNumber("123-45-45621")
+// 				.name("마스터1")
+// 				.phoneNumber("010-1234-1234")
+// 				.email("master1@naver.com")
+// 				.password("Testpassword1@")
+// 				.address("대전시 유성구")
+// 				.license("dkfjlsikdf23155")
+// 				.role(Role.MASTER)
+// 				.build());
+//
 // 		dealerRepository.save(
 // 			Dealer.builder()
 // 				.email("dealer1@naver.com")
@@ -38,6 +45,17 @@
 // 				.phoneNumber("010-1111-1111")
 // 				.password("Testpassword1@")
 // 				.point("강남")
+// 				.role(Role.DEALER)
+// 				.build()
+// 		);
+//
+// 		dealerRepository.save(
+// 			Dealer.builder()
+// 				.email("dealer2@naver.com")
+// 				.name("딜러2")
+// 				.phoneNumber("010-2222-1111")
+// 				.password("Testpassword2@")
+// 				.point("성수")
 // 				.role(Role.DEALER)
 // 				.build()
 // 		);
@@ -52,66 +70,78 @@
 // 				.address("대전시 중구")
 // 				.build()
 // 		);
-// 		modelRepository.save(
-// 			Model.builder()
-// 				.name("쏘나타")
-// 				.color(ModelColor.RED)
-// 				.description("중형 세단으로 탁월한 연비와 편안한 승차감 제공")
-// 				.image("https://example.com/sonata.jpg")
-// 				.category(ModelCategory.EASE)
-// 				.seatCount("5")
-// 				.price(100L)
-// 				.build()
-// 		);
-// 		optionRepository.save(
-// 			Option.builder()
-// 				.name("썬루프")
-// 				.description("차량에 자연광과 개방감을 더해주는 파노라마 썬루프")
-// 				.image("https://example.com/sunroof.jpg")
-// 				.category(OptionCategory.EXTERIOR)
-// 				.price(100L)
-// 				.build()
-// 		);
 //
-// 		optionRepository.save(
-// 			Option.builder()
-// 				.name("네비게이션")
-// 				.description("최신 지도와 경로 안내를 제공하는 인포테인먼트 내비게이션 시스템")
-// 				.image("https://example.com/navigation.jpg")
-// 				.category(OptionCategory.INFOTAINMENT)
-// 				.price(100L)
+// 		userRepository.save(
+// 			User.builder()
+// 				.email("user2@naver.com")
+// 				.name("유저2")
+// 				.phoneNumber("010-2222-1111")
+// 				.password("Testpassword2@")
+// 				.role(Role.USER)
+// 				.dateOfBirth(LocalDate.parse("2000-11-29"))
+// 				.address("대전시 동구")
 // 				.build()
 // 		);
-//
-// 		optionRepository.save(
-// 			Option.builder()
-// 				.name("열선 시트")
-// 				.description("겨울철에도 따뜻하게 유지되는 앞좌석 열선 시트")
-// 				.image("https://example.com/heatedseat.jpg")
-// 				.category(OptionCategory.CONVENIENCE)
-// 				.price(100L)
-// 				.build()
-// 		);
-//
-// 		optionRepository.save(
-// 			Option.builder()
-// 				.name("긴급 제동 시스템")
-// 				.description("충돌 위험 시 자동으로 브레이크를 작동시키는 안전 시스템")
-// 				.image("https://example.com/emergencybrake.jpg")
-// 				.category(OptionCategory.SAFETY)
-// 				.price(100L)
-// 				.build()
-// 		);
-//
-// 		optionRepository.save(
-// 			Option.builder()
-// 				.name("스포츠 서스펜션")
-// 				.description("보다 다이나믹한 주행을 위한 강화 서스펜션")
-// 				.image("https://example.com/sportssuspension.jpg")
-// 				.category(OptionCategory.PERFORMANCE)
-// 				.price(100L)
-// 				.build()
-// 		);
+// 		// modelRepository.save(
+// 		// 	Model.builder()
+// 		// 		.name("쏘나타")
+// 		// 		.color(ModelColor.RED)
+// 		// 		.description("중형 세단으로 탁월한 연비와 편안한 승차감 제공")
+// 		// 		.image("https://example.com/sonata.jpg")
+// 		// 		.category(ModelCategory.EASE)
+// 		// 		.seatCount("5")
+// 		// 		.price(100L)
+// 		// 		.build()
+// 		// );
+// 		// optionRepository.save(
+// 		// 	Option.builder()
+// 		// 		.name("썬루프")
+// 		// 		.description("차량에 자연광과 개방감을 더해주는 파노라마 썬루프")
+// 		// 		.image("https://example.com/sunroof.jpg")
+// 		// 		.category(OptionCategory.EXTERIOR)
+// 		// 		.price(100L)
+// 		// 		.build()
+// 		// );
+// 		//
+// 		// optionRepository.save(
+// 		// 	Option.builder()
+// 		// 		.name("네비게이션")
+// 		// 		.description("최신 지도와 경로 안내를 제공하는 인포테인먼트 내비게이션 시스템")
+// 		// 		.image("https://example.com/navigation.jpg")
+// 		// 		.category(OptionCategory.INFOTAINMENT)
+// 		// 		.price(100L)
+// 		// 		.build()
+// 		// );
+// 		//
+// 		// optionRepository.save(
+// 		// 	Option.builder()
+// 		// 		.name("열선 시트")
+// 		// 		.description("겨울철에도 따뜻하게 유지되는 앞좌석 열선 시트")
+// 		// 		.image("https://example.com/heatedseat.jpg")
+// 		// 		.category(OptionCategory.CONVENIENCE)
+// 		// 		.price(100L)
+// 		// 		.build()
+// 		// );
+// 		//
+// 		// optionRepository.save(
+// 		// 	Option.builder()
+// 		// 		.name("긴급 제동 시스템")
+// 		// 		.description("충돌 위험 시 자동으로 브레이크를 작동시키는 안전 시스템")
+// 		// 		.image("https://example.com/emergencybrake.jpg")
+// 		// 		.category(OptionCategory.SAFETY)
+// 		// 		.price(100L)
+// 		// 		.build()
+// 		// );
+// 		//
+// 		// optionRepository.save(
+// 		// 	Option.builder()
+// 		// 		.name("스포츠 서스펜션")
+// 		// 		.description("보다 다이나믹한 주행을 위한 강화 서스펜션")
+// 		// 		.image("https://example.com/sportssuspension.jpg")
+// 		// 		.category(OptionCategory.PERFORMANCE)
+// 		// 		.price(100L)
+// 		// 		.build()
+// 		// );
 //
 // 	}
 // }
