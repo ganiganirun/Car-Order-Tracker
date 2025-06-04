dfbbe917c013b222d9168372769c5bb11b6e4af8
songgaeun <rkdmsdmqek1128@gmail.com>
2025-06-04 14:56:30 +0900

    feat: 결제 취소 로직 추가

--- src/main/java/com/example/osid/domain/payment/controller/PaymentController.java
+++ src/main/java/com/example/osid/domain/payment/controller/PaymentController.java
@@ -3,12 +3,15 @@ package com.example.osid.domain.payment.controller;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.osid.common.response.CommonResponse;
 import com.example.osid.domain.payment.dto.PaymentRequestDto;
 import com.example.osid.domain.payment.service.PaymentService;
 import com.siot.IamportRestClient.IamportClient;
@@ -23,7 +26,7 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-@RequestMapping("api/v1")
+@RequestMapping("api/payment")
 public class PaymentController {
 
 	@Value("${IMP_API_KEY}")
@@ -41,18 +44,26 @@ public class PaymentController {
 		this.iamportClient = new IamportClient(apiKey, secretKey);
 	}
 
-	@PostMapping("/order/payment/{imp_uid}")
+	@PostMapping("/{imp_uid}")
 	public IamportResponse<Payment> validateIamport(@PathVariable("imp_uid") String impUid,
-		@RequestBody PaymentRequestDto request) throws IamportResponseException, IOException {
+		@RequestBody PaymentRequestDto.Paid request) throws IamportResponseException, IOException {
 
 		IamportResponse<Payment> payment = iamportClient.paymentByImpUid(impUid);
 
 		log.info("결제 요청 응답. 결제 내역 - 주문 번호: {}", payment.getResponse().getMerchantUid());
 
 		// 결제 완료 처리 시, merchantUid로 주문 조회해서 연관관계 연결 가능
-		paymentService.processPaymentDone(request, impUid);
+		paymentService.processPaymentDone(request, payment);
 
 		return payment;
+	}
+
+	@PostMapping("/cancel")
+	public ResponseEntity<CommonResponse> createPayment(@RequestBody PaymentRequestDto.Cancel cancelReq) throws
+		IamportResponseException,
+		IOException {
+		paymentService.cancelReservation(cancelReq);
+		return new ResponseEntity<>(CommonResponse.ok(), HttpStatus.OK);
 	}
 
 }
