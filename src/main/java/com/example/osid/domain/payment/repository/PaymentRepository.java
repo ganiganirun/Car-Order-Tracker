b756805359ffaedaef545c52dedcb2e3f4c07734
songgaeun <rkdmsdmqek1128@gmail.com>
2025-06-04 14:56:15 +0900

    feat: impUid로 payment 검색

--- src/main/java/com/example/osid/domain/payment/repository/PaymentRepository.java
+++ src/main/java/com/example/osid/domain/payment/repository/PaymentRepository.java
@@ -1,9 +1,13 @@
 package com.example.osid.domain.payment.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.osid.domain.payment.entity.Payments;
 
 public interface PaymentRepository extends JpaRepository<Payments, Long> {
+
+	Optional<Payments> findByImpUid(String impUid);
 
 }
