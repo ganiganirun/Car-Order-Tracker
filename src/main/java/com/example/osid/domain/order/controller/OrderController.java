0b824c94cc1d5571a17a017ed714bc8803b68b9e
songgaeun <rkdmsdmqek1128@gmail.com>
2025-06-04 23:53:11 +0900

    refactor: 공통응답 포맷으로 refactor

--- src/main/java/com/example/osid/domain/order/controller/OrderController.java
+++ src/main/java/com/example/osid/domain/order/controller/OrderController.java
@@ -5,7 +5,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +12,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.osid.common.auth.CustomUserDetails;
@@ -31,20 +31,22 @@ public class OrderController {
 
 	// 주문 생성
 	@PostMapping("/api/dealers/order")
-	public ResponseEntity<CommonResponse> createOrder(
+	@ResponseStatus(HttpStatus.CREATED)
+	public CommonResponse<OrderResponseDto.Add> createOrder(
 		@RequestBody OrderRequestDto.Add requestDto,
 		@AuthenticationPrincipal CustomUserDetails customUserDetails
 	) {
 
 		OrderResponseDto.Add order = orderService.createOrder(customUserDetails, requestDto);
 
-		return new ResponseEntity<>(CommonResponse.created(order), HttpStatus.CREATED);
+		return CommonResponse.created(order);
 
 	}
 
 	// 주문 수정
 	@PatchMapping("/api/dealers/order/{orderId}")
-	public ResponseEntity<CommonResponse> updateOrder(
+	@ResponseStatus(HttpStatus.OK)
+	public CommonResponse<OrderResponseDto.Update> updateOrder(
 		@PathVariable Long orderId,
 		@RequestBody OrderRequestDto.Update requestDto,
 		@AuthenticationPrincipal CustomUserDetails customUserDetails
@@ -52,43 +54,46 @@ public class OrderController {
 
 		OrderResponseDto.Update order = orderService.updateOrder(customUserDetails, orderId, requestDto);
 
-		return new ResponseEntity<>(CommonResponse.ok(order), HttpStatus.OK);
+		return CommonResponse.ok(order);
 
 	}
 
 	// 주문 단건 조회
 	@GetMapping("/api/order/{orderId}")
-	public ResponseEntity<CommonResponse> findOrder(
+	@ResponseStatus(HttpStatus.OK)
+	public CommonResponse<Object> findOrder(
 		@PathVariable Long orderId,
 		@AuthenticationPrincipal CustomUserDetails customUserDetails
 	) {
 
 		Object order = orderService.findOrder(customUserDetails, orderId);
 
-		return new ResponseEntity<>(CommonResponse.ok(order), HttpStatus.OK);
+		return CommonResponse.ok(order);
 	}
 
 	// 주문 전체 조회
 	@GetMapping("/api/order")
-	public ResponseEntity<CommonResponse> findOrder(
+	@ResponseStatus(HttpStatus.OK)
+	public CommonResponse<Page<OrderResponseDto.FindAll>> findAllOrder(
 		@AuthenticationPrincipal CustomUserDetails customUserDetails,
 		@PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable
 	) {
 
 		Page<OrderResponseDto.FindAll> order = orderService.findAllOrder(customUserDetails, pageable);
 
-		return new ResponseEntity<>(CommonResponse.ok(order), HttpStatus.OK);
+		return CommonResponse.ok(order);
 	}
 
 	// 주문 삭제
 	@DeleteMapping("/api/dealers/order/{orderId}")
-	public ResponseEntity<CommonResponse> deleteOrder(
+	@ResponseStatus(HttpStatus.OK)
+	public CommonResponse<Void> deleteOrder(
 		@PathVariable Long orderId,
 		@AuthenticationPrincipal CustomUserDetails customUserDetails
 	) {
 		orderService.deleteOrder(customUserDetails, orderId);
 
-		return new ResponseEntity<>(CommonResponse.ok("주문이 삭제 되었습니다."), HttpStatus.OK);
+		return CommonResponse.ok();
 	}
 
 }
