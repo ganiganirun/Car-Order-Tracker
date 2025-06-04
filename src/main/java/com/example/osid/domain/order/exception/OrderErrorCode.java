ebb53ad1fc27b0832a6879364b029b673fe6d66a
songgaeun <rkdmsdmqek1128@gmail.com>
2025-06-04 23:55:21 +0900

    refactor: 예외 코드 수정

--- src/main/java/com/example/osid/domain/order/exception/OrderErrorCode.java
+++ src/main/java/com/example/osid/domain/order/exception/OrderErrorCode.java
@@ -11,8 +11,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum OrderErrorCode implements BaseCode {
 
-	ORDER_NOT_FOUND(HttpStatus.NOT_FOUND, "O001", "해당 주문을 찾을 수 없습니다."),
-	ORDER_ACCESS_DENIED(HttpStatus.FORBIDDEN, "O002", "해당 주문에 대한 접근 권한이 없습니다.");
+	ORDER_NOT_FOUND(HttpStatus.NOT_FOUND, "ORDER_001", "해당 주문을 찾을 수 없습니다."),
+	ORDER_ACCESS_DENIED(HttpStatus.FORBIDDEN, "ORDER_002", "해당 주문에 대한 접근 권한이 없습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String code;
