f1b8d2d4f76674b133e6c54f6f87d0061d1d40ef
songgaeun <rkdmsdmqek1128@gmail.com>
2025-06-10 10:57:22 +0900

    feat :aop 사용을 위한 어노테이션 추가

--- src/main/java/com/example/osid/OsidApplication.java
+++ src/main/java/com/example/osid/OsidApplication.java
@@ -2,10 +2,12 @@ package com.example.osid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableAspectJAutoProxy // AOP 사용을 위함
 public class OsidApplication {
 
 	public static void main(String[] args) {
