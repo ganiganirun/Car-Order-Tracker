3f84a6dfe958fd228a0541c3050b1a10595881f4
se0yu <pr924133@gmail.com>
2025-06-12 12:48:16 +0900

    feat: 이미 삭제된 옵션 재삭제시 에러 출력 1. 관련 로직 생성 2. 관련 에러 생성

--- src/main/java/com/example/osid/domain/option/exception/OptionErrorCode.java
+++ src/main/java/com/example/osid/domain/option/exception/OptionErrorCode.java
@@ -10,7 +10,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum OptionErrorCode implements BaseCode {
-	OPTION_NOT_FOUND(HttpStatus.NOT_FOUND, "O001", "옵션이 없거나 삭제된 상태입니다.");
+	OPTION_NOT_FOUND(HttpStatus.NOT_FOUND, "OPTION_001", "옵션이 없거나 삭제된 상태입니다."),
+	OPTION_ALREADY_DELETED(HttpStatus.CONFLICT, "OPTION_002", "이미 삭제된 옵션입니다.");
 
 	private final HttpStatus httpStatus;
 	private final String code;
