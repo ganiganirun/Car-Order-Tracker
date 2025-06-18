6e15a0620987b5d37239f9a8d82fe04111054753
se0yu <pr924133@gmail.com>
2025-06-18 10:44:41 +0900

    feat: FailedEventResponse 필드(id) 추가, 데이터 포맷 추가 1. FailedEvent 생성일 데이터 포맷 추가

--- src/main/java/com/example/osid/event/dto/FailedEventResponse.java
+++ src/main/java/com/example/osid/event/dto/FailedEventResponse.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 import com.example.osid.event.entity.FailedEvent;
 import com.example.osid.event.enums.FailedEventType;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,21 +13,22 @@ import lombok.Getter;
 @AllArgsConstructor
 public class FailedEventResponse {
 
+	private final Long id;
 	private final Long orderId;
 	private final int retryCount;
 	private final String errorMessage;
 	private final FailedEventType failedEventType;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd-HH:mm:ss")
 	private final LocalDateTime createdAt;
-	private final LocalDateTime updatedAt;
 
 	public static FailedEventResponse from(FailedEvent failedEvent) {
 		return new FailedEventResponse(
+			failedEvent.getId(),
 			failedEvent.getOrderId(),
 			failedEvent.getRetryCount(),
 			failedEvent.getErrorMessage(),
 			failedEvent.getEventType(),
-			failedEvent.getCreatedAt(),
-			failedEvent.getUpdatedAt()
+			failedEvent.getCreatedAt()
 		);
 	}
 }
