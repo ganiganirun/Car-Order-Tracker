596a0237c41eccd5bd1d1b5d01d57a93a315ec51
songgaeun <rkdmsdmqek1128@gmail.com>
2025-06-19 17:42:17 +0900

    feat : 어노테이션 추가
    @EnableScheduling
    @EnableBatchProcessing

--- src/main/java/com/example/osid/OsidApplication.java
+++ src/main/java/com/example/osid/OsidApplication.java
@@ -1,15 +1,19 @@
 package com.example.osid;
 
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableAspectJAutoProxy // AOP 사용을 위함
 @EnableRabbit
+@EnableScheduling
+@EnableBatchProcessing
 public class OsidApplication {
 
 	public static void main(String[] args) {
