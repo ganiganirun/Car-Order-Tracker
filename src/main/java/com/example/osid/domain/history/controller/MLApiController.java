9abcf2b8499abc6bad5ce11c11f36603840fda08
songgaeun <rkdmsdmqek1128@gmail.com>
2025-06-19 17:41:47 +0900

    feat : 외부 api 추가

--- src/main/java/com/example/osid/domain/history/controller/MLApiController.java
+++ src/main/java/com/example/osid/domain/history/controller/MLApiController.java
@@ -1,5 +1,6 @@
 package com.example.osid.domain.history.controller;
 
+import java.time.Duration;
 import java.util.Map;
 
 import org.springframework.http.MediaType;
@@ -8,15 +9,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import io.netty.channel.ChannelOption;
 import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
 
 @RestController
 public class MLApiController {
 
-	// HttpClient hc = HttpClient.create()
-	// 	.responseTimeout(Duration.ofSeconds(25))         // 서버 응답 25s 제한
-	// 	.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000); // TCP 연결 5s 제한
-	//
+	HttpClient hc = HttpClient.create()
+		.responseTimeout(Duration.ofSeconds(25))         // 서버 응답 25s 제한
+		.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000); // TCP 연결 5s 제한
+
 	// WebClient webClient = WebClient.builder()
 	// 	.baseUrl("https://mlp-learning-test.onrender.com")
 	// 	.clientConnector(new ReactorClientHttpConnector(hc))
@@ -34,5 +37,6 @@ public class MLApiController {
 			.bodyValue(input)
 			.retrieve()
 			.bodyToMono(Map.class);
+
 	}
 }
