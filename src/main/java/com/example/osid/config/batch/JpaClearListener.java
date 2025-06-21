9d942ff6fee0fedba71863145e7b0e386481b937
songgaeun <rkdmsdmqek1128@gmail.com>
2025-06-21 20:32:16 +0900

    feat : 클리어리스너 수정

--- src/main/java/com/example/osid/config/batch/JpaClearListener.java
+++ src/main/java/com/example/osid/config/batch/JpaClearListener.java
@@ -5,7 +5,8 @@ import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.stereotype.Component;
 
-import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 
 @Component
@@ -13,10 +14,18 @@ import lombok.RequiredArgsConstructor;
 @StepScope   // 이걸 달면 EMF도 스텝 실행 시점에 프로시(proxy)로 주입됩니다.
 public class JpaClearListener implements ChunkListener {
 
-	private final EntityManagerFactory emf;
+	// private final EntityManagerFactory emf;
+	//
+	// @Override
+	// public void afterChunk(ChunkContext context) {
+	// 	emf.createEntityManager().clear();   // 힙 누수 방지
+	// }
+
+	@PersistenceContext
+	private EntityManager entityManager;
 
 	@Override
 	public void afterChunk(ChunkContext context) {
-		emf.createEntityManager().clear();   // 힙 누수 방지
+		entityManager.clear();
 	}
 }
