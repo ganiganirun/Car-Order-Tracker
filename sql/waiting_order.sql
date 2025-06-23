4b24dbfcb70c7203506602c3e2eccb120f1dcf7c
songgaeun <rkdmsdmqek1128@gmail.com>
2025-06-23 19:15:25 +0900

    feat : 대기열 더미 데이터

--- sql/waiting_order.sql
+++ sql/waiting_order.sql
@@ -5,7 +5,7 @@ SET FOREIGN_KEY_CHECKS = 0;
 TRUNCATE TABLE waiting_orders;
 SET FOREIGN_KEY_CHECKS = 1;
 
-INSERT INTO waiting_orders (order_id, watingStatus, created_at, updated_at)
+INSERT INTO waiting_orders (order_id, waiting_status, created_at, updated_at)
 SELECT o.id, 'WAITING', NOW(), NOW()
 FROM orders o
 WHERE o.id BETWEEN 1 AND 50;
