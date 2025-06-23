d1decb6964e28edbd272bbfc6878f92fd02a87db
songgaeun <rkdmsdmqek1128@gmail.com>
2025-06-23 19:16:56 +0900

    feat : 삭제

--- src/main/java/com/example/osid/domain/waitingorder/repository/WatingOrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.osid.domain.waitingorder.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.osid.domain.waitingorder.entity.WaitingOrders;
-
-public interface WatingOrderRepository extends JpaRepository<WaitingOrders, Long> {
-
-}
