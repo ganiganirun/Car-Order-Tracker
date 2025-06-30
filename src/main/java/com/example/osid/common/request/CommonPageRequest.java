e3b069345fcff3fe0e77a13b62eda119dcca54ed
se0yu <pr924133@gmail.com>
2025-06-30 12:38:31 +0900

    refactor: 공통 pageRequest size 제한 1. 사이즈 1 이상 100 이하로 제한

--- src/main/java/com/example/osid/common/request/CommonPageRequest.java
+++ src/main/java/com/example/osid/common/request/CommonPageRequest.java
@@ -18,9 +18,10 @@ public class CommonPageRequest {
 	private Integer size = 10;
 
 	// 기본 페이지 메서드
+	// 페이지 사이즈 100 이하로 제한
 	public Pageable toPageable() {
 		int finalPage = (page != null && page >= 0) ? page : 0;
-		int finalSize = (size != null && size >= 1) ? size : 10;
+		int finalSize = (size != null && size >= 1 && size <= 100) ? size : 10;
 		return PageRequest.of(finalPage, finalSize, Sort.by(Sort.Direction.DESC, "createdAt"));
 	}
 
