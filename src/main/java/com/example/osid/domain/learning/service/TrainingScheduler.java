75c64d4f115a6b490a1870864be85b2e8c038838
최성원 <90265239+s-woni@users.noreply.github.com>
2025-07-03 16:42:07 +0900

    fix: 5분마다 ML 재학습 => 120분(2시간)마다 재학습

--- src/main/java/com/example/osid/domain/learning/service/TrainingScheduler.java
+++ src/main/java/com/example/osid/domain/learning/service/TrainingScheduler.java
@@ -10,7 +10,7 @@ public class TrainingScheduler {
 
     private final TrainingService trainingService;
 
-    @Scheduled(fixedRate = 5 * 60 * 1000)
+    @Scheduled(fixedRate = 120 * 60 * 1000)
     public void scheduleTrainingAndUpload() {
         System.out.println("[스케줄러] 주기적 학습 및 업로드 시작");
         trainingService.triggerTrainingAndUpload();
